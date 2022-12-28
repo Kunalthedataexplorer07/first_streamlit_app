97b308073720e918507b70b70ca22f7475d175a1
Kunalthedataexplorer07 <121453097+Kunalthedataexplorer07@users.noreply.github.com>
2022-12-28 23:50:15 +0530

    Update streamlit_app.py
    Modified to get response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,9 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 st.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
+st.header('Fruityvice Fruit Advice!')
 
 import requests as req
 
 fruityvice_response = req.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
